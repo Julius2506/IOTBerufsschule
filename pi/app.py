38d887603c996d6a117b6df1aa97fce3fd715e8b
Julius Spannagel <julius.spannagel@quattec.de>
2026-04-28 13:49:29 +0200

    test

--- pi/app.py
+++ pi/app.py
@@ -409,4 +409,4 @@ def api_terrariums():
     return jsonify(terrariums)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
